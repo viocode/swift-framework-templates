942455190b62302674e278403817d5cc0155134a
Tibor Bödecs <mail.tib@gmail.com>
2017-10-23 22:49:20 +0200

    added example usage, license

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Swift framework templates
+
+Example code for the article about how to make frameworks in Swift 4.
+
+* Traditional framework template
+* Universal (cross platform / multi target scheme) template
+* Usage example
+
+### License
+
+[WTFPL](LICENSE)
---
+++ Usage/Usage/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Usage
+//
+//  Created by Tibor Bödecs on 2017. 10. 23..
+//  Copyright © 2017. Tibor Bödecs. All rights reserved.
+//
+
+import UIKit
+import Console
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        Console.log("hello")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
